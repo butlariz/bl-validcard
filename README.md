3a3b3633f7aa96711f2514e2b9286c14e179bfc3
butlariz <laris.santana09@gmail.com>
2018-09-06 11:57:46 -0300

    arquivo eslint, testado no nyc, readme completo, bugs corrigidos

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# Validar Número de Cartão v1.0.0
+# Validar Número de Cartão v1.1.0
 
-Essa biblioteca valida números de cartão. 
+Essa biblioteca se destina a validar números de cartões, utilizando o algoritimo de Luhn, para uso em aplicações web.
+
+## Os métodos utilizados na biblioteca são: 
+
+**cardValidator(num)**
+
+Exemplo de uso:
+
+```
+$node 
+> let validator = require('cardValidator')
+> validator(36490102462661); //true
+``` 
+
+## versão 1.1.0  
+
+- Funcionalidades: verifica se o número de cartão é valido de acordo com o algorítimo de Luhn
+
+## instalação
+
+- Você deverá ter o node e o npm instalados. Para guia de instalação, visite [o site oficial](https://www.npmjs.com/get-npm).
+- Proceda com a instalação com `$npm install bl-validcard`
+
+## roadmap oficial do projeto  
+
+### versão 2.0.0 (sem previsão)
+- Aceitar números com espaço 
+- Verificar qual a bandeira, além de validar
+
+### versão 1.1.0 (released)
+- Funcionalidades: verifica se o número do cartão é válido
+- Aceita números sem espaço
--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 function cardValidator(numberCard) {
-  if (numberCard == "" || !numberCard) {
-    throw Error("Function called with no value")
-  } else if (typeof(numberCard) != "number") {
-    throw Error("Type only numbers")
-  } else if (numberCard.toString().length == 1) {
-    throw Error("One number is not a valid card")
+  if (numberCard === '' || !numberCard) {
+    throw Error('Function called with no value');
+  } else if (typeof(numberCard) !== 'number') {
+    throw Error('Type only numbers');
+  } else if (numberCard.toString().length === 1) {
+    throw Error('One number is not a valid card');
   } else {
-    const arrayNumbers = numberCard.toString().split("").reverse()
-    return luhnAlgorithm(arrayNumbers)
+    const arrayNumbers = numberCard.toString().split('').reverse();
+    return luhnAlgorithm(arrayNumbers);
   } 
 }
 
 const luhnAlgorithm = (array) => {
   arrayLuhn = array.map((value, index) => {
-    value = parseInt(value)
-    return index % 2 == 0 ? value : value * 2 > 9 ? value * 2 - 9 : value * 2
-  })
+    value = parseInt(value);
+    if (index % 2 === 0) {
+      return value;
+    } else {
+      return value * 2 > 9 ? value * 2 - 9 : value * 2;
+    }
+  });
 
-  return sumArray(arrayLuhn)
-}
+  return sumArray(arrayLuhn);
+};
 
 const sumArray = (array) => {
-  let sum = array.reduce((accum, item) => accum + item)
-  return validateSum(sum)
-}
+  let sum = array.reduce((accum, item) => accum + item);
+  return validateSum(sum);
+};
 
 const validateSum = (num) => {
-  return num % 10 == 0
-}
+  return num % 10 === 0;
+};
 
 module.exports.cardValidator = cardValidator;
